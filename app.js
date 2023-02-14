5a3b573f7cb7a4326e97aa277154dfe44d27e55b
e-lamine <lamine@e-lamine.dev>
2023-02-14 11:12:08 +0100

    players can update gameboard

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // game board module
 let gameBoard = (function () {
-   const state = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-   // gameBoard.length = 9;
+   const state = [];
+   state.length = 9;
 
-   function updateGameBoard(symbol) {
-      gameBoard[index] = symbol;
+   function updateGameState(index) {
+      let player = players.find((player) => player.turn === true);
+      state[index] = player.symbol;
    }
 
-   return { state };
+   return { state, updateGameState };
 })();
 
 // display controller
@@ -15,20 +16,48 @@ const displayController = (function () {
    const board = document.getElementById('board');
    for (let i = 0; i < gameBoard.state.length; i++) {
       const square = document.createElement('div');
+      square.classList.add('square');
+      square.dataset.index = i;
       square.innerText = gameBoard.state[i];
       board.appendChild(square);
    }
-})();
 
-// game logic
-let gameEngin = (function () {
-   function alternateTurn() {}
+   function getMoveInfo(e) {
+      const targetSquare = e.target.dataset.index;
+      gameBoard.updateGameState(targetSquare);
+      e.target.innerText = gameBoard.state[targetSquare];
+      gameEngin.alternateTurn();
+      console.log(players, gameBoard);
+   }
 
-   function checkForWinningMove() {}
+   board.addEventListener('click', getMoveInfo);
+
+   // function updateDisplay(e) {
+   //    e.target.innerText = gameBoard.state[targetSquare];
+   // }
 })();
 
 // player factory
-const playerFactory = (name, symbol) => {
-   const play = (symbol) => gameBoard.updateGameBoard(symbol);
-   return { name, symbol, play };
+const playerFactory = (name, symbol, turn) => {
+   const play = () => gameBoard.updateGameBoard(symbol);
+   return { name, symbol, turn, play };
 };
+
+const player01 = playerFactory('amine', 'x', true);
+const player02 = playerFactory('bot', 'o', false);
+
+const players = [player01, player02];
+
+// game logic
+
+let gameEngin = (function () {
+   function alternateTurn() {
+      players.forEach((player) => {
+         player.turn = !player.turn;
+      });
+   }
+
+   function checkForWinningMove() {}
+
+   return { alternateTurn };
+})();
